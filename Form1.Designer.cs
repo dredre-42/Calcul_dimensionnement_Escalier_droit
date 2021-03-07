7815a786f3196a46248f1e8aebc0d37abaa162de
dredre-42 <80153318+dredre-42@users.noreply.github.com>
2021-03-07 15:30:41 +0100

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿
+namespace découverte_Visual_Studio
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblerreur = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblnum = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblll = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblhh = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbll = new System.Windows.Forms.Label();
+            this.lblh = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.boxhcible = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.boxH = new System.Windows.Forms.TextBox();
+            this.calculer = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dessin = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dessin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            this.splitContainer1.Panel1MinSize = 400;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel2);
+            this.splitContainer1.Size = new System.Drawing.Size(1070, 445);
+            this.splitContainer1.SplitterDistance = 498;
+            this.splitContainer1.TabIndex = 18;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblerreur);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.lblnum);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.lblll);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.lblhh);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lbll);
+            this.panel1.Controls.Add(this.lblh);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.boxhcible);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.boxH);
+            this.panel1.Controls.Add(this.calculer);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(498, 445);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblerreur
+            // 
+            this.lblerreur.AutoSize = true;
+            this.lblerreur.Location = new System.Drawing.Point(41, 159);
+            this.lblerreur.Name = "lblerreur";
+            this.lblerreur.Size = new System.Drawing.Size(12, 15);
+            this.lblerreur.TabIndex = 34;
+            this.lblerreur.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(214, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 15);
+            this.label7.TabIndex = 33;
+            this.label7.Text = "nombre de marche";
+            // 
+            // lblnum
+            // 
+            this.lblnum.AutoSize = true;
+            this.lblnum.ForeColor = System.Drawing.Color.Black;
+            this.lblnum.Location = new System.Drawing.Point(358, 253);
+            this.lblnum.Name = "lblnum";
+            this.lblnum.Size = new System.Drawing.Size(50, 15);
+            this.lblnum.TabIndex = 32;
+            this.lblnum.Text = "à définir";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(214, 194);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(119, 15);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "longueur de l\'escalier";
+            // 
+            // lblll
+            // 
+            this.lblll.AutoSize = true;
+            this.lblll.ForeColor = System.Drawing.Color.Black;
+            this.lblll.Location = new System.Drawing.Point(358, 194);
+            this.lblll.Name = "lblll";
+            this.lblll.Size = new System.Drawing.Size(50, 15);
+            this.lblll.TabIndex = 30;
+            this.lblll.Text = "à définir";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 15);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "hauteur de l\'escalier";
+            // 
+            // lblhh
+            // 
+            this.lblhh.AutoSize = true;
+            this.lblhh.ForeColor = System.Drawing.Color.Black;
+            this.lblhh.Location = new System.Drawing.Point(173, 194);
+            this.lblhh.Name = "lblhh";
+            this.lblhh.Size = new System.Drawing.Size(25, 15);
+            this.lblhh.TabIndex = 28;
+            this.lblhh.Text = "300";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(214, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 15);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "longueur d\'une marche";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 15);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "hauteur d\'une marche";
+            // 
+            // lbll
+            // 
+            this.lbll.AutoSize = true;
+            this.lbll.ForeColor = System.Drawing.Color.Black;
+            this.lbll.Location = new System.Drawing.Point(358, 223);
+            this.lbll.Name = "lbll";
+            this.lbll.Size = new System.Drawing.Size(50, 15);
+            this.lbll.TabIndex = 25;
+            this.lbll.Text = "à définir";
+            // 
+            // lblh
+            // 
+            this.lblh.AutoSize = true;
+            this.lblh.ForeColor = System.Drawing.Color.Black;
+            this.lblh.Location = new System.Drawing.Point(175, 223);
+            this.lblh.Name = "lblh";
+            this.lblh.Size = new System.Drawing.Size(19, 15);
+            this.lblh.TabIndex = 24;
+            this.lblh.Text = "15";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(166, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 15);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Valeur en [cm]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 15);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Insérer la hauteur d\'une marhe";
+            // 
+            // boxhcible
+            // 
+            this.boxhcible.Location = new System.Drawing.Point(255, 72);
+            this.boxhcible.Name = "boxhcible";
+            this.boxhcible.Size = new System.Drawing.Size(74, 23);
+            this.boxhcible.TabIndex = 21;
+            this.boxhcible.Text = "15";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 15);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Insérer la hauteur de l\'escalier de fini à fini";
+            // 
+            // boxH
+            // 
+            this.boxH.Location = new System.Drawing.Point(255, 28);
+            this.boxH.Name = "boxH";
+            this.boxH.Size = new System.Drawing.Size(74, 23);
+            this.boxH.TabIndex = 19;
+            this.boxH.Text = "300";
+            // 
+            // calculer
+            // 
+            this.calculer.Location = new System.Drawing.Point(255, 114);
+            this.calculer.Name = "calculer";
+            this.calculer.Size = new System.Drawing.Size(74, 23);
+            this.calculer.TabIndex = 18;
+            this.calculer.Text = "Valider";
+            this.calculer.UseVisualStyleBackColor = true;
+            this.calculer.Click += new System.EventHandler(this.calculer_Click);
+
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dessin);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(568, 445);
+            this.panel2.TabIndex = 0;
+            // 
+            // dessin
+            // 
+            this.dessin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dessin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dessin.Location = new System.Drawing.Point(0, 0);
+            this.dessin.Name = "dessin";
+            this.dessin.Size = new System.Drawing.Size(568, 445);
+            this.dessin.TabIndex = 17;
+            this.dessin.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1070, 445);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calcul escalier";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dessin)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblerreur;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblnum;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblll;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblhh;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbll;
+        private System.Windows.Forms.Label lblh;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox boxhcible;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox boxH;
+        private System.Windows.Forms.Button calculer;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox dessin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace découverte_Visual_Studio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+    
+
+
+        }
+
+        private void calculer_Click(object sender, EventArgs e)
+        {
+            lancerCalcul();
+        }
+
+        void dessiner(double h, double l, int num)
+        {
+            // le premier point est en bas à gauche
+            //      mais x = 0 et y = num 
+            // le deuxième point = l'axe y du premier point + h
+            // le 3ème = l'axe x du 2ème + l
+            // faire ça le nombre de marche donc num
+            // dessiner en reliant tout ses points
+
+            Bitmap image = new Bitmap(dessin.Width, dessin.Height);
+            using(Graphics g = Graphics.FromImage(image))
+            {
+
+                //int i = 0;
+                //while int i < num
+                //      h += h
+
+
+                Point ptOrigin = new Point(0, dessin.Height);
+                Point ptavant = ptOrigin;
+
+                string err = "Bonjour" + " André";
+                string err2 = "truc";
+                err += " !!!!!" + err2;
+                lblerreur.Text = err;
+
+
+                for (int i = 0; i < num; i++)
+                {
+
+
+                    Point pt1 = ptavant;
+
+                    Point pt2 = new Point(pt1.X, Convert.ToInt32(Math.Round(pt1.Y - h, 0)));
+
+                    Point pt3 = new Point(Convert.ToInt32(Math.Round(pt2.X + l, 0)), pt2.Y);
+
+                    g.DrawLine(new Pen(Color.Black), pt1.X, pt1.Y, pt2.X, pt2.Y);
+                    g.DrawLine(new Pen(Color.Black), pt2.X, pt2.Y, pt3.X, pt3.Y);
+
+                    if (i != 0)
+                    {
+                        ptavant = pt3;
+                    }
+                
+
+                }
+
+
+
+            }
+
+            dessin.Image = image;
+        }
+        void lancerCalcul()
+        {
+            double H = 0;
+            double hcible = 0;
+
+            if (boxhcible.Text == "")
+            {
+                hcible = 15;
+            }
+            else if (double.TryParse(boxhcible.Text, out hcible) == false)
+            {
+                hcible = 15;
+            }
+
+            else
+            {
+                hcible = Convert.ToDouble(this.boxhcible.Text);
+            }
+
+            if (boxH.Text == "" || double.TryParse(boxH.Text, out H) == false)
+            {
+                H = 300;
+            }
+            else
+            {
+                H = Convert.ToDouble(this.boxH.Text);
+            }
+
+            calculerEscalier(H, hcible);
+
+
+        }
+
+        void calculerEscalier(double H, double hcible)
+        {
+          
+            // Calcul
+            //définir le nombre de marche - num
+            //définir la hauteur de marche - h
+            //définir la largueur de marche - l
+            //63=2h+l
+            int num = Convert.ToInt32(Math.Round(H / hcible, 0));
+            double h = H / num;
+            double l = 63 - (2 * hcible);
+            double L = l * num;
+
+
+            // h hauteur d'une marche
+            //l longueur d'une marche
+            //nom nombre de marches
+            lblh.Text = h.ToString();
+            lblhh.Text = H.ToString();
+            lbll.Text = l.ToString();
+            lblll.Text = L.ToString();
+            lblnum.Text = num.ToString();
+
+            dessiner(h, l, num);
+        }
+
+        private void Form1_ResizeEnd(Object sender, EventArgs e)
+
+        {
+            lancerCalcul();
+        }
+
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbll_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boxhcible_TextChanged(object sender, EventArgs e)
+        {
+            //lancerCalcul();
+        }
+        private void boxH_TextChanged(object sender, EventArgs e)
+        {
+          //  lancerCalcul();
+        }
+
+    }
+}
